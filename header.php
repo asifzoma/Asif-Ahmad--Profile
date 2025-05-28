f3697f779bb3f22ebdb38fc9925d48b69e1e27b7
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-05-28 22:12:06 +0100

    header sidebar to php
    Created a new header.php file containing <head>, opening <body>, and sidebar markup.
    Updated index.php to use <?php include 'header.php'; ?> replacing  header/sidebar code.

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asif Ahmad</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script src="js/script.js" defer></script>
+</head>
+<body>  
+    <div class="page-wrapper">
+        <!-- Hamburger Menu -->
+        <button class="hamburger-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+
+        <!-- Sidebar Container -->
+        <nav id="navigator"class="sidebar-container">
+            <div class="sidebar-header">
+                <h3><a href="#about-accordion">AZA</a></h3>
+            </div>
+            <div class="sidebar-menu">
+                <ul>
+                    <li class="nav-tab"><a href="#about-accordion">About Me</a></li>
+                    <li class="nav-tab"><a href='index.php#portfolio'>My Portfolio</a></li>
+                    <li class="nav-tab"><a href="#code-snippets">Code Snippets</a></li>
+                    <li class="nav-tab"><a href="scs-scheme.php#SCS-scheme">SCS Scheme</a></li>
+                    <li class="nav-tab"><a href="#contact">Contact Me</a></li>
+                </ul>
+            </div>
+            <div class="sidebar-social">
+                <a href="https://github.com/asifzahma"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/asifzahmad/"><i class="fab fa-linkedin"></i></a>
+                <a href="https://discord.com/users/AsifZahmad"><i class="fab fa-discord"></i></a>
+            </div>
+        </nav> 
