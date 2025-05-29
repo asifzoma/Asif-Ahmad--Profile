f851bf2d96a8647eb89388dd4d6cd4d1b8dd53a8
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-05-29 08:40:03 +0100

    update portfolio section
    Moved entire <section class="portfolio">...</section> block from index.php into  new file called portfolio-section.php.
    Replace that section in index.php with <?php include 'portfolio-section.php'; ?>.

--- index.php
+++ index.php
@@ -76,27 +76,7 @@
             
 
             <!-- Portfolio Section -->
-            <section class="portfolio">
-                <h2 id="portfolio">My Portfolio</h2>
-                <div class="portfolio-container">
-                    <div class="project-card" data-full-desc="Cloning Netmatters website to assess and improve my HTML, CSS and JavaScript skills. As part of the project I refactored the website from HTML to PHP, added a SQL database and a working contact form, which is connected to the database. Capturing the data from the form and storing it in the database. I have also ensured that the news cards and data on the Website can be updated by the admin.">
-                        <img src="img/Netmatters_refactor_project.png" alt="Netmatters Refactor Project">
-                        <h3>Netmatters Clone Project</h3>
-                        <p>Cloning Netmatters website to improve my skills...</p>
-                        <a href="https://netmatters.asif-ahmad.netmatters-scs.co.uk/" class="btn">View the live site</a>
-                    </div>
-                    <div class="project-card" data-full-desc="This is a placeholder project. Add your full project description here for the modal.">
-                        <img src="img/placeholder.jpg" alt="Project Placeholder">
-                        <h3>Project Title</h3>
-                        <p>Project description goes here...</p>
-                    </div>
-                    <div class="project-card" data-full-desc="This is another placeholder project. Add your full project description here for the modal.">
-                        <img src="img/placeholder.jpg" alt="Project Placeholder">
-                        <h3>Project Title</h3>
-                        <p>Project description goes here...</p>
-                    </div>
-                </div>
-            </section>
+            <?php include 'portfolio-section.php'; ?>
 
             <!-- Code Snippets Section -->
             <section class="code-snippets">
