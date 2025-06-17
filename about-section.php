0dc4af8a614c420981c3db2ab5f310b529e218d9
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-06-17 15:20:47 +0100

    changes to scs cards
    scs cards now display 2 on top and one stretched at certain vp widths

--- about-section.php
+++ about-section.php
@@ -6,10 +6,10 @@
                 <section class="about-me" id="about-me">
                     <h2 class="about-me-header">About Me</h2>
                     <p>
-                        I'm Asif, a Full Stack Web Developer passionate about designing and developing dynamic, user-friendly applications.
+                        I'm Asif, a Full Stack Web Developer passionate about designing and developing aesthetic, user-friendly websites and applications.
                     </p>
-                    <p>My journey into web development began with a genuine curiosity about technology and a desire to create digital solutions that make a real difference. I have developed a strong foundation in both front-end and back-end development, with hands-on experience in HTML, CSS, JavaScript, and PHP.</p>
-                    <p>I am committed to building efficient, scalable, and innovative web applications that solve real-world problems. My experience spans the entire development lifecycle, from designing intuitive user interfaces to implementing robust server-side logic.</p>
-                    <p>Beyond coding, I am passionate about business strategy, problem-solving, and user experience design. These interests enable me to deliver well-rounded applications that provide tangible value to users and stakeholders. I am eager to contribute my skills and enthusiasm to a forward-thinking team, and I look forward to collaborating on impactful projects that drive business success.</p>
+                    <p>My journey into web development began with a genuine curiosity about technology, I also enjoy technological problem solving. I have a strong foundation in both front-end and back-end development, with hands-on experience in HTML, CSS, JavaScript, and PHP.</p>
+                    <p>I enjoy building efficient, secure, scalable, and innovative web applications that solve real-world problems, or simply make life easier. My experience spans the entire development lifecycle, from designing user interfaces to implementing server-side logic.</p>
+                    <p>Beyond coding, I am passionate about business strategy, problem-solving, and user experience design. These interests enable me to deliver well-rounded applications that provide tangible value to users and stakeholders. I am eager to contribute my skills and enthusiasm to a forward-thinking team, and I look forward to potentially working with you in the future.</p>
                 </section>
             </section> 
